318cb407d5e250aff78212665b98ae05ddf0a199
Austin Trow <austintrow@gmail.com>
2019-06-21 00:46:29 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,2 +1,2 @@
-$(document).ready(function() {$("#text").append("this is so freaking cool.");
+$(document).ready(function() {$("#text").append("this is so freaking cool. \n Boy to I have to poop reeeeeeeal bad.");
 });
